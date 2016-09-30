3451e1935ffa82c23e7c03c7b56b4e61dac0a65c
Matt Walter <mwalter@mwalter-macbookpro2.roam.corp.google.com>
2016-09-30 14:12:51 -0700

    Beginning of lecture 79

---
+++ TravelCollector/TravelViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TravelViewController.swift
+//  TravelCollector
+//
+//  Created by Matt Walter on 9/30/16.
+//  Copyright © 2016 Matt Walter. All rights reserved.
+//
+
+import UIKit
+
+class TravelViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var travelImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+    }
+    @IBAction func photosTapped(_ sender: AnyObject) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+      let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        travelImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+    
+}
